d80e015fee276b49d0d742debcd1b02ea1d08ba9
“CAPTAIN-JOB” <job3kiptoo@gmail.com>
2025-01-07 23:26:26 +0300

    Routes

--- server/index.js
+++ server/index.js
@@ -2,10 +2,23 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+const pool = require("./database")
+
+
 
 app.use(cors())
 app.use(express.json())
 
+//routes//
+// Create a Todo
+// Get all Todos
+// Get a Todo
+// Update a Todo
+// Delete a Todo
+
 app.listen(5000, () => {
 	console.log("server has started on port 5000")
 })
+
+
+
